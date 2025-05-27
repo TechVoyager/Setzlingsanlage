20fd95d26afd9b3042b617392a1a2de33143cc86
Jacke1702 <jakob.braunger@gmx.de>
2025-05-27 18:42:46 +0200

    Unterklasse hinzugefügt

--- Raspberry Pico/Aktoren.py
+++ Raspberry Pico/Aktoren.py
@@ -2,21 +2,33 @@
 
 from abc import ABC, abstractmethod
 
+#Abstrakte Klasse der Aktoren
 class Actors(ABC):
-        @abstractmethod
-        def __innit__(self, name, location, unit):
+        def __init__(self, name, pin, unit):  #kein @abstractmethod, da ich ja sonst bei jeder Unterklasse nen Init deklarieren müsste
                 self.name = name
-                self.location = location
+                self.pin = pin
                 self.unit = unit
+                self.state = False   #False weil ich es ja IMMER auf False setze, wäre quatsch auf State zu setzen
 
         @abstractmethod
         def on(self): #alle Klassen der Aktoren müssen An und Aus gehen
             pass
-
+        
         @abstractmethod
         def off(self): 
-               pass
+            self.state = False
+            pass
+
+#Unterklasse Aktoren:
+class fan(Actors):
+    def on(self):
+        self.state = True
+        print("Hallo")
+    def off(self):
+        self.state = False
+        print(self.pin)
+            
         
-
-
-
+lufter = fan("lufter", 2, "l")
+lufter.on()
+    
