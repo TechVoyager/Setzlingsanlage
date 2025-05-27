51f02e844ea5acd1a33eabcc0cb6eef9fc1410f4
Jacke1702 <jakob.braunger@gmx.de>
2025-05-27 11:34:23 +0200

    Aktoren deklariert

--- Raspberry Pico/Aktoren.py
+++ Raspberry Pico/Aktoren.py
@@ -1,1 +1,22 @@
 # Modul für sämtliche Aktor-Klassen
+
+from abc import ABC, abstractmethod
+
+class Actors(ABC):
+        @abstractmethod
+        def __innit__(self, name, location, unit):
+                self.name = name
+                self.location = location
+                self.unit = unit
+
+        @abstractmethod
+        def on(self): #alle Klassen der Aktoren müssen An und Aus gehen
+            pass
+
+        @abstractmethod
+        def off(self): 
+               pass
+        
+
+
+
