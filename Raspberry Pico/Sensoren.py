5a5103d69fc8982b1c64aa904c1c59cd886a9c56
Jacke1702 <jakob.braunger@gmx.de>
2025-05-27 11:09:48 +0200

    abstrakte Klasse für Sensoren deklariert

--- Raspberry Pico/Sensoren.py
+++ Raspberry Pico/Sensoren.py
@@ -1,1 +1,12 @@
 # Modul für sämtliche Sensor-Klassen
+from abc import ABC, abstractmethod
+
+class Sensor(ABC):
+        @abstractmethod
+        def __innit__(self, name, location, unit):
+                self.name = name
+                self.location = location
+                self.unit = unit
+
+        def masure(self): #alle Unterklassen müssen die Funktion Messen haben!
+            pass
