c7159649ad2d7d6bbe68e348e0e987cb33460831
Jacke1702 <jakob.braunger@gmx.de>
2025-06-16 12:56:29 +0200

    auf Circuit Pytgon umgeändert Fab hinzugefügt

--- Raspberry Pico/Aktoren.py
+++ Raspberry Pico/Aktoren.py
@@ -1,34 +1,53 @@
-# Modul für sämtliche Aktor-Klassen
-
-from abc import ABC, abstractmethod
+import board
+import digitalio
+import time
 
 #Abstrakte Klasse der Aktoren
-class Actors(ABC):
+class Actors():
         def __init__(self, name, pin, unit):  #kein @abstractmethod, da ich ja sonst bei jeder Unterklasse nen Init deklarieren müsste
                 self.name = name
                 self.pin = pin
                 self.unit = unit
                 self.state = False   #False weil ich es ja IMMER auf False setze, wäre quatsch auf State zu setzen
 
-        @abstractmethod
         def on(self): #alle Klassen der Aktoren müssen An und Aus gehen
-            pass
+            raise NotImplementedError("Jeder Aktor muss angeschaltet werden!!")
         
-        @abstractmethod
         def off(self): 
             self.state = False
-            pass
+            raise NotImplementedError("Jeder Aktor muss angeschaltet werden!!")
 
 #Unterklasse Aktoren:
-class fan(Actors):
+class Fan(Actors):
     def on(self):
         self.state = True
-        print("Hallo")
+        print("Lüfter läuft")
     def off(self):
         self.state = False
-        print(self.pin)
-            
-        
-lufter = fan("lufter", 2, "l")
-lufter.on()
+        print("Lüfter hört auf")
+
+class waterpump(Actors):
+    def on(self):
+        self.state = True
+        print("Wasserpumpe läuft")
+    def off(self):
+        self.state = False
+        print("Wasserpumpe hört auf")    
+
+
+class Wateratomizer(Actors):
+    def on(self):
+        self.state = True
+        print("Wasserzerstäuber läuft")
+    def off(self):
+        self.state = False
+        print("Wasserzerstäuber hört auf")   
+
+#class Heatingmat(Actors):
+   
+
+#class Light(Actors):
+    
+
+
     
