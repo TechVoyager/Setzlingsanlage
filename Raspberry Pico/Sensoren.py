fa917f9041e48b5ce5f721ae79ca5314562683cc
Jacke1702 <jakob.braunger@gmx.de>
2025-06-16 12:45:09 +0200

    PID-Regler

--- Raspberry Pico/Sensoren.py
+++ Raspberry Pico/Sensoren.py
@@ -1,9 +1,10 @@
-# Modul für sämtliche Sensor-Klassen
+"""# Modul für sämtliche Sensor-Klassen
 #from abc import ABC, abstractmethod  Funktioniert nur auf dem PC!!!!
 from machine import ADC, Pin  #funktioniert nur auf MicroPython!!! nicht auf dem PC mit normalem Python
 from time import ticks_ms, ticks_diff 
 #ticks_ms() gibt die aktuellen Millisekunden seit start zurück
 #ticks_diff(a, b) #berechnet den Unterschied zw. zwei zeitpunkten
+import dht #für DHT11 Temperatur und FTK Sensor 
 
 class Sensor():
         def __init__(self, name, location, unit, interval_ms):
@@ -36,7 +37,7 @@ class Sensor():
 #Sensor gibt 0= 0V bis 65535 = 3,3V zurück
 #mit ADC (analog_Digital-Converter)
 class Soilmoisturemeter(Sensor): #Bodenfeuchtigkeitsmesser
-        
+
         def __init__(self, location, pin_gp, interval_ms):
                 super().__init__("Soilmoisturemeter", location, "%", interval_ms)  #Die Einheit(unit) ist in Prozent angegeben #super macht, dass man nicht erneut die Elternklasse initialisieren muss also self.name z.B., da wir ja die Einheit festlegen)
                 self.adc = ADC(Pin(pin_gp)) #ist zwar am Pin 31 aber ist GP26
@@ -46,18 +47,49 @@ class Soilmoisturemeter(Sensor): #Bodenfeuchtigkeitsmesser
                percent = (raw / 65535) * 100  #wandelt die Zahl 0-65535 in Prozent um 
                return round(percent, 1) #wird noch auf eins gerundet
         
+#DHT11 Temperatur und Feuchtigkeistssensor:
+#flache seite aufm Tisch: VCC(3,3V oder 5V) ganz links; Data(Signal) in der Mitte; GND (Masse) ganz rechts
+#Funktion: VCC bekommt Strom, Data sendet/empfängt. Beim ruhezustand fließt trotdem 3,3 V, mit nem Widerstand wird der Pin20 vor zu starkem Strom geschützt. 
+# Der Sensor ist digital: Bei 0V wird data mit gnd verbunden... er schickt impulse 
 
-sensor = Soilmoisturemeter("POS:A", 26, 10000)
+class Temperature_humidity_sensor(Sensor): #Temperature and humidity sensor
+
+        def __init__(self, location, pin_gp, interval_ms):
+                super().__init__("Temperature_humidity_sensor", location, "%", interval_ms)
+                self.sensor = dht.DHT11(Pin(pin_gp)) #Pin(15) bedeutet nutze den GPIO15 als Eingang, dht.DHT11 erzeugt ein Objekt, das weiß wie man mit DHT11 kommunizieren muss
+                
+        def measure(self):
+                self.sensor.measure() #Sensor startet Messung
+                temperature = self.sensor.temperature() #Temperatur in °C
+                humidity = self.sensor.humidity() #Luftfeuchtigkeit in %
+                return {"temperature": temperature, "humidity": humidity} #gibt dict. zurück mit Luftfeuchtigkeit und Temperatur
+
+sensor = Temperature_humidity_sensor("POS:B", 15, 10000)
+sensor2 = Soilmoisturemeter("POS:A", 29, 10000)
+sensors = [sensor, sensor2]
+
 
 while True:
-        if sensor.should_measure(): 
-                value = sensor.measure()  #Methode Messen aufrufen
-                print(f"{sensor.name} bei {sensor.location}: {value} {sensor.unit}") #Ausgabe der Werte
-                sensor.update_timestamp()
+        for s in sensors:
+                if s.should_measure(): 
+                        value = s.measure()  #Methode Messen aufrufen
+                        print(f"{s.name} bei {s.location}: {value} {s.unit}") #Ausgabe der Werte
+                        s.update_timestamp()
 
+"""
+from machine import Pin
+import dht
+from time import sleep
 
-               
+sensor = dht.DHT11(Pin(15))  # GPIO15
 
-               
+sleep(2)
 
-
+while True:
+    try:
+        sensor.measure()
+        print("Temperatur:", sensor.temperature(), "°C")
+        print("Luftfeuchtigkeit:", sensor.humidity(), "%")
+    except Exception as e:
+        print("Fehler:", e)
+        sleep(2)
--- Raspberry Pico/main.py
+++ Raspberry Pico/main.py
@@ -1,6 +1,83 @@
-# Hauptprogramm
+# Hauptprogramm 
+import time
 
-from Aktoren import *
+#from Aktoren import *
 from Sensoren import *
-from Pflanzenprofil import *
-from SerInterface import *
+#from Pflanzenprofil import *
+#from SerInterface import *
+
+#Sensoren belegung
+sensor1 = Temperature_humidity_sensor("POS:B", board.GP15, 3)
+sensor2 = Soilmoisturemeter("POS:A", board.GP28, 3)  #physisch pin 34
+sensors = [sensor1, sensor2]
+
+#Regelung muss nich eingestellt werden!
+#Temperatur
+pid_temp = PID(kp=1.0, ki=0.1, kd=0.05, setpoint=25) #Sollwert 25Grad
+#Bodenfeuchtigkeit
+pid_soil = PID(kp=1.0, ki=0.1, kd=0.05, setpoint=50) #Sollwert Luftfeuchtigkeit 50 Prozent
+#Luftfeuchtigkeit
+pid_air = PID(kp=1.0, ki=0.1, kd=0.05, setpoint=30) #Sollwert Bodenfeuchtigkeit 30 Prozent
+
+#Setpoint = Sollwert; kp = proportional; ki = Interval nähert sich; kd achtet auf die Fehler der Zeit
+class PID:
+    def __init__(self, kp, ki, kd, setpoint):
+        self.kp = kp   
+        self.ki = ki
+        self.kd = kd
+        self.setpoint = setpoint
+
+        self.last_error = 0
+        self.integral = 0
+        self.lasttime = time.monotonic()
+
+    def compute(self, measured_value):
+        current_time = time.monotonic()           #aktuelle Zeit
+        delta = current_time - self.lasttime    #Zeitdiff...
+        error = self.setpoint - measured_value  #Fehler
+
+        self.integral += error * delta          #Summe aller Fehler während der Zeit
+
+        if delta > 0: #Ableitung berechnen
+            derivative = (error - self.last_error) / delta #Fehlerdifff geteilt durch Zeitdiff
+        else:
+            derivative = 0
+
+        #PID Regelwert berechnen:
+        p_value = self.kp * error
+        i_value = self.ki * self.integral 
+        d_value = self.kd * derivative
+        output = p_value + i_value + d_value
+
+        #aktuelle Werte wieder abspeichern
+        self.last_error = error 
+        self.lasttime = current_time
+
+        return output #regelgröße damit z.B. wenn größer als 50 ist werden aktoren angesteuert
+ 
+#Sensoren mit zeitabstand auslesen:
+#value is None: Schutzmaßnahme falls sensor bissle spinnt
+while True:
+        for s in sensors:
+                if s.should_measure(): 
+                        value = s.measure()  #Methode Messen aufrufen
+                        print(f"{s.name} bei {s.location}: {value} {s.unit}") #Ausgabe der Werte
+                        s.update_timestamp()
+                        
+                        if value is None:   
+                            continue    
+                        #Optimierungsmöglichkeit: max Fehler
+
+                        if s.name == "Temperature_humidity_sensor":
+                            temp = value["temperature"]
+                            hum = value["humidity"]
+                            output_temp = pid_temp.compute(temp)
+                            output_hum = pid_air.compute(hum)
+                            print(f"PID temp output: {output_temp}, PID hum output: {output_hum}")
+
+                        elif s.name == "Soilmoisturemeter":
+                            output_soil = pid_soil.compute(value)
+                            print(f"PID Soil Moisture Output: {output_soil}")
+
+
+#vielleicht noch min und max Output
