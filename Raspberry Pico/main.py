b125a63c3df0d091487e260a4ff08b3a74296337
Anle0903 <anna-lena-schill@gmx.de>
2025-07-04 18:45:01 +0200

    Struktur erarbeitet

--- Raspberry Pico/main.py
+++ Raspberry Pico/main.py
@@ -7,30 +7,13 @@
 import time
 import board
 
-from Aktoren import *
-from Sensoren import *
+# Exakte Klassen importieren, nicht automatisch alles
 from Pflanzenprofil import *
 from SerInterface import *
-from Suche import*
+from Suche import *
 from pid import PID 
-from hardware_setup import sensor_temp, sensor_soil, fan1, fan2, atomizer, pumpe, sensors
-#Regelung muss nich eingestellt werden!
-#Temperatur
-pid_temp = PID(kp=1.0, ki=0.1, kd=0.05, setpoint=25) #Sollwert 25Grad
-pid_temp.min_output = 0
-pid_temp.max_output = 100
+from hardware_setup import sensor_temp, sensor_soil, fan_inward, fan_outward, atomizer, pump, lampfan, sensors, heatingmat, light
 
-#Bodenfeuchtigkeit
-pid_soil = PID(kp=1.0, ki=0.1, kd=0.05, setpoint=50) #Sollwert Luftfeuchtigkeit 50 Prozent
-pid_soil.min_output = 0
-pid_soil.max_output = 100
-
-#Luftfeuchtigkeit
-pid_air = PID(kp=1.0, ki=0.1, kd=0.05, setpoint=30) #Sollwert Bodenfeuchtigkeit 30 Prozent
-pid_air.min_output = 0
-pid_air.max_output = 100
-
-#Setpoint = Sollwert; kp = proportional; ki = Interval nähert sich; kd achtet auf die Fehler der Zeit
 
 #Klassenelemente erstellen
 plantprofile = Pflanzenprofil()
@@ -38,22 +21,53 @@ plantprofile = Pflanzenprofil()
 #Variablen
 #Variablen zu Pflanzenarten
 plantspecies = "Erdbeeren" # wird geändert durch GUI
-seed = True # seed == True : Samen -- seed== False: Setzling
+seed = True # seed == True : Samen -- seed == False: Setzling
             # wird geändert durch GUI
-plantvalues_dict = plantprofile.gib_Pflanzenwerte(plantspecies, seed)
-last_update_plantvalues = time.monotonic() # Zahl in s
-update_time_plantvalues = 5 * 24 * 60 * 60 # Tage in Sekunden
+target_values = plantprofile.gib_Pflanzenwerte(plantspecies, seed)
+time_since_start = time.monotonic() # Zahl in s -> neuer Wert zuweisen wenn neue Pflanzenart ausgewählt wurde
 
+update_time_plantvalues = 5 
+cur_meassurements = {"air_temperature": 25, "air_humidity": 50, "soil_temperature": 25, "soil_moisture": 50 }
+pid_values = {"air_temperature": 0, "air_humiditiy": 0, "soil_temperature": 0, "soil_moisture": 0}
+
+#Regelung muss nich eingestellt werden!
+#Temperatur
+
+pid_soil_temp = PID(kp=1.0, ki=0.1, kd=0.05, setpoint=target_values["Bodentemperatur_tag"]) #Sollwert 25Grad
+pid_soil_temp.min_output = 0
+pid_soil_temp.max_output = 100
+
+#Bodenfeuchtigkeit
+pid_soil_moisture = PID(kp=1.0, ki=0.1, kd=0.05, setpoint=target_values["Bodenfeuchte"]) #Sollwert Luftfeuchtigkeit 50 Prozent
+pid_soil_moisture.min_output = 0
+pid_soil_moisture.max_output = 100
+number_of_days = 0
+#Luftfeuchtigkeit
+pid_humidity = PID(kp=1.0, ki=0.1, kd=0.05, setpoint=target_values["Luftfeuchte"]) #Sollwert Bodenfeuchtigkeit 30 Prozent
+pid_humidity.min_output = 0
+pid_humidity.max_output = 100
+
+#Setpoint = Sollwert; kp = proportional; ki = Interval nähert sich; kd achtet auf die Fehler der Zeit
 
 #Sensoren mit zeitabstand auslesen:
 #value is None: Schutzmaßnahme falls sensor bissle spinnt
 #Daten speichern 
 while True:
+        """
+        1. Sensoren auslesen
+        2. Sensorwerte verarbeiten -> PID-Wert erstellen
+        3. Aktoren ansteuern
+        4. Kommunikation mit GUI
+        """
         # nach 5 Tagen neue Werte für die Pflanze ins dict schreiben (von Samen zu Setzling)
-        # seed muss dann noch aktualisier werden durchs GUI
-        current_time_plantvalues = time.monotonic()
-        if (current_time_plantvalues - last_update_plantvalues) > update_time_plantvalues:
-              plantvalues_dict = plantprofile.gib_Pflanzenwerte(plantspecies, seed)
+        if plantspecies == "manuell":
+             target_values = manuel_values
+        else:
+            if number_of_days >= update_time_plantvalues:
+                target_values = plantprofile.gib_Pflanzenwerte(plantspecies, seed=False)
+            else:
+                target_values = plantprofile.gib_Pflanzenwerte(plantspecies, seed=True)
+        # Sensoren auslesen
         for s in sensors:
                 if s.should_measure(): 
                         value = s.measure()  #Methode Messen aufrufen
@@ -67,13 +81,62 @@ while True:
                         if s.name == "Temperature_humidity_sensor":
                             temp = value["temperature"]
                             hum = value["humidity"]
-                            output_temp = pid_temp.compute(temp)  #PID Wert von 0-100
-                            output_hum = pid_air.compute(hum)     #PID Wert von 0-100
-                            print(f"PID temp output: {output_temp}, PID hum output: {output_hum}")
+                            cur_meassurements["air_temperature"] = temp
+                            cur_meassurements["air_humidity"] = hum
+                            pid_values["air_humiditiy"] = pid_humidity.compute(hum)     #PID Wert von 0-100
+                            print(f"PID temp output: {pid_values["air_temperature"]}, PID hum output: {pid_values["air_humidity"]}")
 
                         elif s.name == "Soilmoisturemeter":
-                            output_soil = pid_soil.compute(value)
-                            print(f"PID Soil Moisture Output: {output_soil}")
+                            cur_meassurements["soil_moisture"] = value
+                            pid_values["soil_moisture"] = pid_soil_moisture.compute(value)
+                            print(f"PID Soil Moisture Output: {pid_values["soil_moisture"]}")
+
+                        elif s.name == "SoilTemperaturesensor":
+                            cur_meassurements["soil_temperature"] = value
+                            pid_values["soil_temperature"] = pid_soil_temp.compute(value)
+                            print(f"PID Soil Temperature Output: {pid_values["soil_temperature"]}")
 
 
+        # TO-DO: Schwellenwerte sind NICHT KORREKT, MÜSSEN ANGEPASST WERDEN
+        if pid_values["soil_moisture"] > 50:
+            pump.on()
+        else:
+            pump.off()
+
+        # TO-DO: Schwellenwerte sind NICHT KORREKT, MÜSSEN ANGEPASST WERDEN
+        if pid_values["soil_temperature"] > 50:
+            heatingmat.on()
+        else:
+            heatingmat.off()
+        
+        # TO-DO: Schwellenwerte sind NICHT KORREKT, MÜSSEN ANGEPASST WERDEN
+        if pid_values["air_humiditiy"] > 50:
+            fan_inward.on()
+            fan_outward.on()
+        else:
+            fan_inward.off()
+            fan_outward.off()
+
+        # TO-DO: Schwellenwerte sind NICHT KORREKT, MÜSSEN ANGEPASST WERDEN
+        if pid_values["air_humiditiy"] < 50:
+            atomizer.on()
+        else:
+            atomizer.off()
+        
+        cur_time_light = time.monotonic()
+        if ((cur_time_light - time_since_start) - number_of_days*24*60*60) > target_values["Tagdauer"] * 60 * 60:
+             # Wird ausgeführt, wenn der Tag vorbei ist
+             light.off()
+        if (((cur_time_light - time_since_start) - number_of_days*24*60*60) - target_values["Tagdauer"] * 60 * 60) > target_values["Nachtdauer"] * 60 * 60:
+             # Wird ausgeführt, wenn die Nacht vorbei
+             light.on()
+             number_of_days += 1
+
+        # HIER JETZT: Kommunikation mit GUI
+
+
+
+# mit pid_values["air_temperature"] -> Zersteuber und Lüfter regeln
+
+# PID-Wert kann auch negativ sein!
 
