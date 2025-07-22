56832936325edb71f569c5e2561f66dbf23046a3
TechVoyager <nicolas.smolka@gmail.com>
2025-07-22 20:18:29 +0200

    Berechnung der Bodentemperatur korrigiert

--- Raspberry Pico/Sensoren.py
+++ Raspberry Pico/Sensoren.py
@@ -3,7 +3,7 @@ import analogio #geändert für machine
 import adafruit_dht 
 import time 
 import math
-imoort analogio
+import analogio
 #abstrakte Klasse:
 class Sensor():
         def __init__(self, name, location, unit, interval_s):
@@ -45,34 +45,40 @@ class Soilmoisturemeter(Sensor): #Bodenfeuchtigkeitsmesser
                 super().__init__("Soilmoisturemeter", location, "%", interval_s)  #Die Einheit(unit) ist in Prozent angegeben #super macht, dass man nicht erneut die Elternklasse initialisieren muss also self.name z.B., da wir ja die Einheit festlegen)
                 self.adc = analogio.AnalogIn(pin)
         def measure(self):
-               raw = self.adc.value  #Raspberry Pi Pico hat 12 Bit ADC aber MircoPython skaliert auf 16 Bit hoch #Rohwert (0-65535 lesen)
+               raw = self.adc.value  # Machine.ADC hat die 4056 auf 16 Bit hochskaliert, weshalb anfangs der Wert auf 65535
                percent = (raw / 65535) * 100  #wandelt die Zahl 0-65535 in Prozent um 
                return round(percent, 1) #wird noch auf eins gerundet
         
 #NTC = temperaturabhängig = hohe Temperatur hoher Widerstand
-class SoilTemperaturesensor(Sensor): #Bodentemperatur: rknown = Referenzwiderstand(100kOhm), beta = Materialkonstante des NTC, t0 = Referenztemperatur in Grad
-        def __init__(self, location, pin, interval_s, adc_max=65535, r_known = 92000, beta=6965, t0=25):
+class SoilTemperaturesensor(Sensor): #Bodentemperatur, beta = Materialkonstante des NTC, reference_voltage = Refernzspannung 3.3V; t0 = Referenztemperatur in Grad, Widerstand2 = 100 kOhm, Referenzspannung = 
+        def __init__(self, location, pin, interval_s, r_known = 92000, beta=6965, t0=25):
                 super().__init__("SoilTemperaturesensor", location, "°C", interval_s)  #Die Einheit(unit) ist in Prozent angegeben #super macht, dass man nicht erneut die Elternklasse initialisieren muss also self.name z.B., da wir ja die Einheit festlegen)
                 self.adc = analogio.AnalogIn(pin) #digitaler Wert zwischen 0 und 65535
-                self.adc_max = adc_max
+                self.adc_max = 65536
+                self.resistance2 = 100000 #in Ohm Widerstand mit RNTC in Reihe geschalten
                 self.r_known = r_known
                 self.beta = beta
                 self.t0_kelvin = t0 + 273.15
+                self.reference_voltage = 3.315
         #aktueller Widerstand des NTC Sensors
         def adc_to_resistance(self, adc_value):
                 if adc_value == 0:
                         return float("inf") #unendlicher Widerstand
-                return self.r_known * (self.adc_max / adc_value -1) #Spannungsteiler ber. des Widerstandes Rntc
+                print("adc: ", adc_value)
+                voltage = adc_value * (self.reference_voltage / (2**16 -1)) +0.03
+                print("Spannung: ", voltage)
+                return self.resistance2 / (self.reference_voltage - voltage)
 
 
         #berechnet die Temperatur in grad 
         def resistance_to_temp(self, resistance):
                 if resistance == 0 or resistance == float("inf"): #wenn Wackelkontakt
                         return None #keinen gültigen Wert
+                print("widerstand: ", resistance)
                 ln = math.log(resistance / self.r_known)   #Teil der Beta Formel
                 inv_t = (1/self.t0_kelvin) + (1 / self.beta) *ln #inverse temperature = Kehrwert der Temperatur in Kelvin
                 t_kelvin = 1 / inv_t
-                return t_kelvin - 273.15
+                return t_kelvin - 273.15 -4 
 
 
         def measure(self):
